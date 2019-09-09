1759c8a5821d4a7454b07708aa58284b091c0f52
EC2 Default User <ec2-user@ip-10-0-0-204.eu-west-1.compute.internal>
2019-09-09 16:02:57 +0000

    update app.py

--- src/app.py
+++ src/app.py
@@ -8,4 +8,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World! Welcome to demo-app'
+    return '<!DOCTYPE html> \
+<html lang="en"> \
+<head> \
+<title>Page Title</title> \
+<meta charset="UTF-8"> \
+<meta name="viewport" content="width=device-width, initial-scale=1"> \
+<style> \
+ \
+ \
+/* Header/logo Title */ \
+.header { \
+  padding: 80px; \
+  text-align: center; \
+  background: #1abc9c; \
+  color: white; \
+} \
+ \
+/* Increase the font size of the heading */ \
+.header h1 { \
+  font-size: 40px; \
+} \
+ \
+ \
+</style> \
+</head> \
+<body> \
+<div class="header"> \
+  <h1>ECS Fargate demo website</h1> \
+  <p>A <b>simple hello</b> from ECS Fargate</p> \
+</div> \
+</body> \
+</html>  '  
